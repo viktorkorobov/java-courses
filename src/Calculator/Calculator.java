422a78a11a24e132373c9bd20b87f61a4a7864d7
viktorkorobov <40566605+viktorkorobov@users.noreply.github.com>
2018-06-26 21:31:36 +0300

    Добавил две переменные, и сложил их

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,7 +1,12 @@
 package Calculator;
-
+import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Calculator: ");
+        Scanner scan = new Scanner(System.in);
+            System.out.println("Calculator: ");
+                int first = scan.nextInt();
+                int second = scan.nextInt();
+                int sum= first + second;
+            System.out.println("Sum: "+sum);
     }
 }
